f7497222c2b5db13546519962754648bc5ac727e
zcnzdmr <ozcanozdemir001@gmail.com>
2024-04-02 22:02:03 +0300

    background color is changed

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor.systemYellow
         let screenWidth = UIScreen.main.bounds.width
         
         //Label kısmı
